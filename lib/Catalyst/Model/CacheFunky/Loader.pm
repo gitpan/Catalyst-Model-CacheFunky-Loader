b7d313a3f1abb245239489d1f15d33467c352803
Tomohiro Teranishi <tomyhero@cpan.org>
2008-04-06 03:18:26 +0000

    Import of TOMYHERO/Catalyst-Model-CacheFunky-Loader-0.05 from CPAN.
    gitpan-cpan-distribution: Catalyst-Model-CacheFunky-Loader
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         TOMYHERO/Catalyst-Model-CacheFunky-Loader-0.05.tar.gz
    gitpan-cpan-author:       TOMYHERO
    gitpan-cpan-maturity:     released

--- lib/Catalyst/Model/CacheFunky/Loader.pm
+++ lib/Catalyst/Model/CacheFunky/Loader.pm
@@ -7,34 +7,35 @@ use NEXT;
 use Module::Recursive::Require;
 use Carp;
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 sub new {
-    my $self = shift->NEXT::new(@_);
-    my $c    = shift;
-    my $class = ref( $self ) ;
+    my $self  = shift->NEXT::new(@_);
+    my $c     = shift;
+    my $class = ref($self);
 
-    my $funky_class     = $self->{class};
-    my $funky_config    = $self->{initialize_info};
-    my $mrr_args        = $self->{mrr_args} || {};
+    my $funky_class  = $self->{class};
+    my $funky_config = $self->{initialize_info};
+    my $mrr_args     = $self->{mrr_args} || {};
 
-    croak 'You must set class and initialize_info' unless $funky_class && $funky_config ;
+    croak 'You must set class and initialize_info'
+        unless $funky_class && $funky_config;
 
-    my @funkies
-        = Module::Recursive::Require->new( $mrr_args )->require_by( $funky_class ); 
+    my @funkies = Module::Recursive::Require->new($mrr_args)
+        ->require_by($funky_class);
 
     no strict 'refs';
-    for my $funky ( @funkies ) {
+    for my $funky (@funkies) {
 
-        $funky->setup( %{ $funky_config } );
+        $funky->setup( %{$funky_config} );
 
-        my $funky_short = $funky;   
+        my $funky_short = $funky;
         $funky_short =~ s/^$funky_class\:\://g;
         my $classname = "${class}::$funky_short";
-        
-        *{"${funky}::context"} = sub { $c };
+
+        *{"${funky}::context"} = sub {$c};
         *{"${classname}::ACCEPT_CONTEXT"} = sub {
-            return $funky ; 
+            return $funky;
         };
     }
 
@@ -47,62 +48,60 @@ sub new {
 
 Catalyst::Model::CacheFunky::Loader - Load Cache::Funky Modules.
 
-=head1 SYNOPSYS
+=head1 SYNOPSIS
 
- package MyApp::Model::Funky;
-
- use strict;
- use warnings;
- use base qw/Catalyst::Model::CacheFunky::Loader/;
- 
- __PACKAGE__->config(
-    class            => 'MyApp::CacheFunky', # read all module under MyApp::CacheFunky::*
-    initialize_info  => { 'Storage::Simple' => {} } ,
-    mrr_args         => { path => '/var/www/Common/lib/' } , # option. SEE L<Module::Recursive::Require> new(\%args) 
- );
-
- 1;
-
- package MyApp::CacheFunky::Foo;
-
- use strict;
- use warnings;
- use qw/Cache::Funky/;
- 
- __PACKAGE__->register( 'foo' , sub { `date`} );
-
- 1;
-
-
- package MyAPpCacheFunky::Users;
-
- use strict;
- use warnings;
- use qw/Cache::Funky/;
-
- __PACKAGE__->register( 'user_count' , sub { __PACKAGE__->context()->model('DB::Users')->count(); } );
-
- 1;
-
-
-
- package MyApp::Controller::FooBar;
-
- sub foo : Local {
-    my ( $s ,$c ) = @_;
-
-    $c->log->debug( $c->model('Funky::Foo')->foo() );
-    sleep(1);
-    $c->log->debug( $c->model('Funky::Foo')->foo() );
-    sleep(1);
-    $c->model('Funky::Foo')->delete('foo');
-    $c->log->debug( $c->model('Funky::Foo')->foo() );
- }
-
- 1;
-
- [% c.forward('Model::Funky::Foo' ,'foo' ) %]
-
+    package MyApp::Model::Funky;
+    
+    use strict;
+    use warnings;
+    use base qw/Catalyst::Model::CacheFunky::Loader/;
+    
+    __PACKAGE__->config(
+        class => 'MyApp::CacheFunky', # read all module under MyApp::CacheFunky::*
+        initialize_info => { 'Storage::Simple' => {} },
+        mrr_args => { path => '/var/www/Common/lib/' } , # option. SEE L<Module::Recursive::Require> new(\%args)
+    );
+    
+    1;
+    
+    package MyApp::CacheFunky::Foo;
+    
+    use strict;
+    use warnings;
+    use qw/Cache::Funky/;
+    
+    __PACKAGE__->register( 'foo', sub {`date`} );
+    
+    1;
+    
+    package MyAPpCacheFunky::Users;
+    
+    use strict;
+    use warnings;
+    use qw/Cache::Funky/;
+    
+    __PACKAGE__->register( 'user_count',
+        sub { __PACKAGE__->context()->model('DB::Users')->count(); } );
+    
+    1;
+    
+    package MyApp::Controller::FooBar;
+    
+    sub foo : Local {
+        my ( $s, $c ) = @_;
+    
+        $c->log->debug( $c->model('Funky::Foo')->foo() );
+        sleep(1);
+        $c->log->debug( $c->model('Funky::Foo')->foo() );
+        sleep(1);
+        $c->model('Funky::Foo')->delete('foo');
+        $c->log->debug( $c->model('Funky::Foo')->foo() );
+    }
+    
+    1;
+    
+    [ %c . forward( 'Model::Funky::Foo', 'foo' ) % ]
+    
 =head1 DESCRIPTION
 
 Load L<Cache::Funky> modules and make them ready for you.
@@ -118,6 +117,12 @@ L<Module::Recursive::Require>
 =head1 AUTHOR
 
 Tomohiro Teranishi <tomohiro.teranishi@gmail.com>
+
+=head1 COPYRIGHT AND LISENCE
+
+Copyright (c) Tomohiro Teranishi, All rights reserved.
+
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.  See L<perlartistic>.
 
 =cut
 
--- t/lib/TestApp.pm
+++ t/lib/TestApp.pm
@@ -13,7 +13,7 @@ use Catalyst::Runtime '5.70';
 # Static::Simple: will serve static files from the application's root 
 #                 directory
 
-use Catalyst qw/ConfigLoader/;
+use Catalyst qw/-Debug ConfigLoader/;
 
 our $VERSION = '0.01';
 
